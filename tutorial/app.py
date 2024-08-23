183a07f492a0900835b8c08d6a478714eb5aadd7
SoyVitouPro <vitoupro.vtp@gmail.com>
2024-08-23 15:56:28 +0700

    update deployment

--- tutorial/app.py
+++ tutorial/app.py
@@ -1,24 +1,14 @@
-# app.py
 from flask import Flask, request, jsonify
 import joblib
 
 app = Flask(__name__)
-
-# Load the trained model
-model = joblib.load('./random_forest_model.pkl')
+model = joblib.load('random_forest_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()  # Expecting JSON format
-    if not data or 'features' not in data:
-        return jsonify({'error': 'No features provided'}), 400
-
-    features = data['features']
-    try:
-        prediction = model.predict([features])
-        return jsonify({'prediction': prediction.tolist()})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    data = request.json
+    prediction = model.predict([data['features']])
+    return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
